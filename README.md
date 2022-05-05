539b94a8546a08aab1abf3c69476c0b136961c59
Jarad DeLorenzo <jdelorenzo@justworks.com>
2022-05-05 14:24:30 -0400

    Added readme

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # JustworksFrontEndChallenge
-Created with CodeSandbox
+
+### Problem
+You are tasked with making a simple payroll entry system for your new company, Justmocks.
+It shall list all current employess and allow the user to enter an amount to pay (in cents)
+The system will keep track of the total to be paid out on Payday.
+
+### Requirements
+1. Each user shall be listed on a single page (no pagination necessary)
+2. Each user row shall include an input to type in the amount in cents owed
+3. The bottom of the list shall include a `total` that is dynamically calculated as the entries are typed in
+
+* Do not focus on design or layout
+* Create as many components as you think necessary
+* Show as many react concepts as you feel comfortable with (useEffect, useRef, useMemo, useCallback, etc...)
+* (optional) Add tests as you see fit
--- src/EmployeeList.js
+++ src/EmployeeList.js
@@ -3,21 +3,24 @@ import { useEffect, useState } from "react";
 export default function EmployeeList() {
   let [users, setUsers] = useState([]);
 
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((response) => {
-        setUsers(response);
-      });
-  }, []);
+  /**
+   * To get a list of employees, make a GET request to:
+   * "https://jsonplaceholder.typicode.com/users"
+   *
+   * hint (remove this?):
+   * fetch("https://jsonplaceholder.typicode.com/users")
+   *
+   * Or put full example?
+   *     fetch("https://jsonplaceholder.typicode.com/users")
+   *       .then((response) => response.json())
+   *       .then((response) => {
+   *         setUsers(response);
+   *       });
+   */
 
   return (
     <div className="EmployeeList">
-      <ul>
-        {users.map((user, i) => {
-          return <li id="user-#{i}">{user.name}</li>;
-        })}
-      </ul>
+    {/*  List employees here ! */}
     </div>
   );
 }
